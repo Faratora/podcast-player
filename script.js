08aa375898b0848983fad54c32e355fbc4d5174c
Svetlana <brigantina@mail.ru>
2026-07-06 21:45:23 +0300

    feat: add localStorage methods for state persistence   (Mon Jul 2026-07-06 21:45:06)

--- script.js
+++ script.js
@@ -25,3 +25,30 @@ class AppState {
         this.audioPlayer = new Audio();
         this.audioPlayer.preload = 'metadata';
     }
+
+    loadPlaylist() {
+    try {
+        const data = localStorage.getItem('podcast_playlist');
+        return data ? JSON.parse(data) : [];
+    } catch {
+        return [];
+    }
+}
+
+savePlaylist() {
+    localStorage.setItem('podcast_playlist', JSON.stringify(this.playlist));
+}
+
+loadProgress() {
+    try {
+        const data = localStorage.getItem('podcast_progress');
+        return data ? JSON.parse(data) : {};
+    } catch {
+        return {};
+    }
+}
+
+saveProgress(episodeId, position) {
+    this.playbackProgress[episodeId] = position;
+    localStorage.setItem('podcast_progress', JSON.stringify(this.playbackProgress));
+}
